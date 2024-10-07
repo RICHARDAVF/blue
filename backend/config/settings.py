8143285c94ddcb9aaed2c706e004e55428ddd3dc
RICHARDAVF <richarferro1308@gmail.com>
2024-10-07 12:48:21 -0500

    se esta agrenado static

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,14 +76,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 # ASGI_APPLICATION = 'config.asgi.application'
-CHANNEL_LAYERS = {
-    "default":{
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        "CONFIG":{
-            "hosts":[('127.0.0.1',6379)],
-        }
-    }
-}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
@@ -130,8 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/'),
 
+STATICFILES_DIR =[os.path.join(BASE_DIR,'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from blue_ventas.core.login.views import LoginView,LoginCliente
+from blue_ventas.views import index
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
     path('api/',include("blue_ventas.core.api.urls")),
     path('login/',LoginView.as_view()),
     path('login/cliente/',LoginCliente.as_view()),
 
 ]
+urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
